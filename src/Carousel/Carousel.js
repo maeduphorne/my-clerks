ecc1bd01899a47a88653e68e37718e865a3ace74
Mae Duphorne <maelduphorne@gmail.com>
2021-11-21 11:48:56 -0700

    Set up onClick for Next button

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -3,6 +3,10 @@ import UserProfileCard from '../UserProfileCard/UserProfileCard'
 
 const Carousel = ({ userInfo }) => {
 
+  const handleNextClick = () => {
+    console.log('I am clicking')
+  }
+
   const cards = userInfo.map((info) => {
     return (
       <UserProfileCard key={info.id} userInfo={info}/>
@@ -15,10 +19,17 @@ const Carousel = ({ userInfo }) => {
       <div className="profile-card-container">
         {cards}
       </div>
-      <button className="next">NEXT</button>
+      <button className="next" onClick={() => handleNextClick()}>NEXT</button>
     </section>
   )
 
 }
 
 export default Carousel;
+
+// CAROUSEL component
+  // should map and display correct USERPROFILECARDs from userInfo state
+    //on load, the carousel displays the initial 1 card (grow to 3 for desktop)
+      // if there are card sin localStorage, grab those and display
+      // if no cards in local storage, fetch new
+    // may need to hold index in state to go back to index once the fetch has happened
