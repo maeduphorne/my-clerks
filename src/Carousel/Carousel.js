d16cf60266ea2616d99fad0c9a651bd66f287f2c
Mae Duphorne <maelduphorne@gmail.com>
2021-11-21 12:02:55 -0700

    Add currentCard hook

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -4,9 +4,14 @@ import { useState } from 'react';
 
 const Carousel = ({ userInfo }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentCard, setCurrentCard] = useState({})
+
+  
 
   const handleNextClick = () => {
     console.log('I am clicking')
+    setCurrentIndex(currentIndex + 1)
+    // currentCard = userInfo[currentIndex]
   //NEXT BTN function 
   // carousel index - starts at 0 on initial page load
   // with each click of the next button, 1 is added to the index
@@ -15,17 +20,19 @@ const Carousel = ({ userInfo }) => {
   // return to view of card at previous index
   }
 
-  const cards = userInfo.map((info) => {
-    return (
-      <UserProfileCard key={info.id} userInfo={info}/>
-    )
-  })
+  // const cards = currentCard.map((info) => {
+  //   return (
+  //     <UserProfileCard key={info.id} userInfo={info}/>
+  //   )
+  // })
 
   return(
     <section className="carousel">
       <button className="prev">PREV</button>
       <div className="profile-card-container">
-        {cards}
+        {/* {currentCard} */}
+        {/* <UserProfileCard key={currentCard.id} userInfo={currentCard}/> */}
+        CARD CONTAINER
       </div>
       <button className="next" onClick={() => handleNextClick()}>NEXT</button>
     </section>
