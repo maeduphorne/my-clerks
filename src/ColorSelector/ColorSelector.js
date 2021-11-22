4ac3d9eb9f014f336fb1aabc76e0f71c37430ad9
Mae Duphorne <maelduphorne@gmail.com>
2021-11-21 18:53:48 -0700

    Write method to create color options

--- src/ColorSelector/ColorSelector.js
+++ src/ColorSelector/ColorSelector.js
@@ -1,7 +1,14 @@
 import './ColorSelector.css'
 
 const ColorSelector = ({ colors, selectedColor, setSelectedColor }) => {
-
+  const colorNames = colors.map(color => {
+    return (
+      <option 
+        >{color.name}
+      </option> 
+    )
+  })
+  
 }
 
 export default ColorSelector;
