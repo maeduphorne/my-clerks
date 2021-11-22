388476c1a74d66d237197ca35dcadbfa6e116370
Mae Duphorne <maelduphorne@gmail.com>
2021-11-21 21:43:05 -0700

    pass prop to carousel for screensize

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,13 +1,14 @@
 import './Carousel.css'
 import UserProfileCard from '../UserProfileCard/UserProfileCard'
 
-const Carousel = ({ currentCard, color }) => {
+const Carousel = ({ carouselStyle, currentCard, color }) => {
   // const cards = currentCard.map((info) => {
   //   return (
   //     <UserProfileCard key={info.id} userInfo={info}/>
   //   )
   // })
-
+  console.log('carouselStyle', carouselStyle)
+  
   return(
     <section className="carousel">
       <UserProfileCard key={currentCard.id} userInfo={currentCard} color={color}/>
