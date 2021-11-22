e80a8fe5e7df2eb5e6b68eeedfec81e01c237903
Mae Duphorne <maelduphorne@gmail.com>
2021-11-21 23:00:16 -0700

    Remove resize prop

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -1,13 +1,12 @@
 import './Carousel.css'
 import UserProfileCard from '../UserProfileCard/UserProfileCard'
 
-const Carousel = ({ carouselStyle, currentCard, color }) => {
+const Carousel = ({ currentCard, color }) => {
   // const cards = currentCard.map((info) => {
   //   return (
   //     <UserProfileCard key={info.id} userInfo={info}/>
   //   )
   // })
-  console.log('carouselStyle', carouselStyle)
   
   return(
     <section className="carousel">
